dc1ba6ec6098a091b513162c58a9499fce84ec80
Nidakhairunnisa <Nidakhairunnisa28@gmail.com>
2022-03-24 11:07:24 +0700

    menambahkan file contacts

---
+++ pertemuan_4.contact_code/contacts.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// membuat folder data apabila tidak ada
+const dirpath= './data';
+if(!fs.existsSync(dirpath)){
+    fs.mkdirSync(dirpath);
+}
+
+//membuat file contats json jika belum ada
+const dataPath = './data/contacts.json';
+if(!fs.existsSync(dataPath)){
+    fs.writeFileSync(dataPath,'[]','utf-8');
+}
+
+// make a function to ask
+const question = (ask) => {
+    return new Promise((resolve,rect) =>{
+        rl.question(ask, (inputVariable)=>{
+            resolve(inputVariable);
+        });
+    });
+};
+module.exports.question = question;
+
+const main = async () =>{
+    const name = await question('What is your name ? ');
+    const mobile = await question('your mobile phone ? ');
+    const email = await question('your email ? ');
+    const contact = {name, mobile, email};
+    const file = fs.readFileSync('data/contacts.json', 'utf8');
+    const contacts =JSON.parse(file);
+    contacts.push(contact);
+    fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
+    console.log('Terima kasih sudah memasukkan data!');
+    rl.close();
+}
+
+main();
